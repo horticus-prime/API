7b9d413231aa5270cb1e3a3bf4fe146e5a95cff3
ekearm <ed787.ed@gmail.com>
2019-06-26 20:28:37 -0700

    got get and post tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,15 +3,10 @@
 process.env.STORAGE = 'mongo';
 
 const server = require('../src/app.js').server;
+
 const supergoose = require('./supergoose.js');
 
 const mockRequest = supergoose.server(server);
-
-let moistureData = {
-  timestamp: {type: String, required: true},
-  moistureCategory: {type: String, required: true},
-  moistureNumber: {type: String, required: true},
-}
 
 beforeAll(supergoose.startDB);
 afterAll(supergoose.stopDB);
@@ -25,8 +20,4 @@ describe('api server', () => {
       });
   });
 });
-
-// describe('The information from the database', () => {
-//   it('Should retrieve ')
-// });
 
