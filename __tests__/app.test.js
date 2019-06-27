dbf4680995dfc7336d8c0c3921c3e4bb476dd134
ekearm <ed787.ed@gmail.com>
2019-06-26 20:41:32 -0700

    almost finished with testing getting tired

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,5 +19,34 @@ describe('api server', () => {
         expect(results.status).toBe(404);
       });
   });
+
+  it('should be able to post to a valid model', ()  => {
+
+    let data = {timestamp:'07/11/2011', moistureCategory: 'WET', moistureNumber: '789'};
+    return mockRequest
+      .post('/moisture')
+      .send(data)
+      .then(results => {
+        expect(results.status).toBe(200);
+        expect(results.body).toEqual(data);
+      });
+  });
+
+
+  it('following a post to a valid model, should find a single record', () => {
+    let data = {timestamp:'07/11/2011', moistureCategory: 'WET', moistureNumber: '789'};
+    return mockRequest
+      .post('/moisture')
+      .send(data)
+      .then(results => {
+        return mockRequest.get(`/moisture/${results.body._id}`)
+          .then(list => {
+            expect(list.status).toBe(200);
+            expect(list.body).toEqual(data);
+          });
+      });
+  });
+
+
 });
 
