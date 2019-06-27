a6236cfbda21f1bc765ebc67b5704f4abe6919b7
Chloie Parsons <chloieparsons@gmail.com>
2019-06-27 10:40:56 -0700

    Cleaned up unused code and working on tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,8 @@
 'use strict';
 
 process.env.STORAGE = 'mongo';
-
 const server = require('../src/app.js').server;
-
 const supergoose = require('./supergoose.js');
-
 const mockRequest = supergoose.server(server);
 
 beforeAll(supergoose.startDB);
@@ -20,33 +17,14 @@ describe('api server', () => {
       });
   });
 
-  it('should be able to post to a valid model', ()  => {
+  it('should not be able to post to a invalid model', ()  => {
 
-    let data = {timestamp:'07/11/2011', moistureCategory: 'WET', moistureNumber: '789'};
+    let data = {timestamp:'13/1011/35', moistureCategory: 'Muddy', moistureNumber: '53490'};
     return mockRequest
-      .post('/moisture')
+      .post('/dirt')
       .send(data)
       .then(results => {
-        expect(results.status).toBe(200);
-        expect(results.body).toEqual(data);
+        expect(results.status).toBe(404);
       });
   });
-
-
-  it('following a post to a valid model, should find a single record', () => {
-    let data = {timestamp:'07/11/2011', moistureCategory: 'WET', moistureNumber: '789'};
-    return mockRequest
-      .post('/moisture')
-      .send(data)
-      .then(results => {
-        return mockRequest.get(`/moisture/${results.body._id}`)
-          .then(list => {
-            expect(list.status).toBe(200);
-            expect(list.body).toEqual(data);
-          });
-      });
-  });
-
-
 });
-
